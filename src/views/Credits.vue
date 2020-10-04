ab4b4e1201e8753e476a574296168991d8e4fb0b
Mattia Righetti <matt95.righetti@gmail.com>
2020-10-04 12:25:49 +0200

    Credits page implementation and minor fixes

---
+++ src/views/Credits.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="px-5 md:px-15 lg:px-20 xl:px-64 pt-10 pb-20">
+        <p class="text-3xl mb-5">Credits</p>
+        <div class="container mb-5">
+            <p class="text-2xl mb-2">Conceptual Structure</p>
+            <p class="text-lg">Daniela Molinari - Department of Civil and Environmental Engineering at Politecnico di Milano</p>
+            <p class="text-lg">Francesco Ballio - Department of Civil and Environmental Engineering at Politecnico di Milano</p>
+            <p class="text-lg">Irene Bombelli - Politecnico di Milano</p>
+        </div>
+
+        <div class="container mb-5">
+            <p class="text-2xl mb-2">Database Filling</p>
+            <p class="text-lg">Daniela Molinari - Department of Civil and Environmental Engineering at Politecnico di Milano</p>
+            <p class="text-lg">Irene Bombelli - Politecnico di Milano</p>
+        </div>
+
+        <div class="container mb-5">
+            <p class="text-2xl mb-2">Design and Implementation</p>
+            <p class="text-lg">Irene Bombelli - Politecnico di Milano</p>
+            <p class="text-lg">Mattia Righetti - Politecnico di Milano</p>
+        </div>
+    </div>
+</template>
