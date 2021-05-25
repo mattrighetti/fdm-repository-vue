338bd75cb93d2e1559e4d6bf1de92abe7becb73f
Mattia Righetti <matt95.righetti@gmail.com>
2021-05-25 03:15:50 +0200

    Landing page with image background

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,13 +1,18 @@
 <template>
-    <div class="container mx-auto mt-5 sm:mt-20">
-        <div class="text-center text-5xl  font-extrabold leading-none tracking-tight">
-            <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-900 to-blue-700">
-                Flood Damage Model Repository
-            </span>
-        </div>
-        <div class="flex justify-center mt-5 sm:mt-20 sm">
-            <div class="text-gray-700 text-center text-base sm:text-2xl px-4 py-2 m-2">
-                Supporting flood damage modelers in the choice of the best available model for a specific country and a specific problem
+    <div class="container mx-auto my-5 sm:mt-20">
+        <div class="relative">
+            <img class="rounded-lg" src="https://media.npr.org/assets/img/2017/11/09/addicks-flooding-1_custom-e57e4f11c64db7d91f401ea381bf211f09b207b3-s1600-c85.webp" alt="flood-1" height="100">
+            <div class="flex justify-center absolute bottom-0 mb-24">
+                <div class="w-3/4 rounded-xl bg-white bg-opacity-75">
+                    <div class="text-center text-5xl font-extrabold leading-none tracking-tight my-8">
+                        <span class="text-black">
+                            Flood Damage Model Repository
+                        </span>
+                    </div>
+                    <div class="text-gray-900 text-center text-base sm:text-2xl px-8 py-2 m-2">
+                        Supporting flood damage modelers in the choice of the best available model for a specific country and a specific problem
+                    </div>
+                </div>
             </div>
         </div>
     </div>
